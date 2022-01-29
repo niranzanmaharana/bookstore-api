77293f78c5f885b9bf4803f03fdb7a1f677942c5
Niranjan Maharana <niranzan.in@gmail.com>
2022-01-29 22:09:23 +0530

    initial commit

---
+++ src/main/java/org/niranzan/bookstore/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package org.niranzan.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
